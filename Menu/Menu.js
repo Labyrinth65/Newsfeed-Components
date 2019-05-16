d20136724b2b882de8eb12d746401f365bdefe27
Ang Xu <angxu1234@gmail.com>
2019-05-15 22:10:43 -0500

    stretch

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,20 @@
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
-  menu.classList.toggle("menu--open");
+  // menu.classList.toggle("menu--open");
+  if (menu.classList.contains("active")) {
+    TweenMax.to(menu, 0.15, { width: 0 });
+    TweenMax.to(menuList, 0.15, { opacity: 0 });
+    menu.classList.remove("active");
+  } else {
+    TweenMax.to(menu, 0.15, { width: 1200 });
+    TweenMax.to(menuList, 0.15, { opacity: 1 });
+    menu.classList.add("active");
+  }
 };
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector(".menu");
+const menuList = menu.querySelector("ul");
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector(".menu-button");
 // Using your menuButton reference, add a click handler that calls toggleMenu
